661c0df3124fe53a1a2397e38195c754c00313bc
Ebinejar Nagubamu <nagubamu.ebinejar@tanla.com>
2026-08-15 14:27:14 +0530

    added the correct database url

---
+++ backend/app/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+DATABASE_URL = "postgresql://employee_user:employee_password@localhost:5432/employee_db"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
