1b1816f937a66a11d93583bdff31bf5933f86033
Ebinejar Nagubamu <nagubamu.ebinejar@tanla.com>
2026-08-15 16:11:49 +0530

    main py chnages are correctly added now for frontend now

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 from .database import Base, engine, get_db
@@ -7,6 +8,17 @@ from .models import Employee
 app = FastAPI(
     title="Employee Management API",
     version="1.0.0",
+)
+
+# CORS configuration
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://44.211.185.97:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 Base.metadata.create_all(bind=engine)
